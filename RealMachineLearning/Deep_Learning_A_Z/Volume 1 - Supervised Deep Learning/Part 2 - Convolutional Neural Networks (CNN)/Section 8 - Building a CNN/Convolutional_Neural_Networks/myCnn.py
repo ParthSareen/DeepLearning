d41ce7c60c46ad35505d12e3004d4300d07def55
ParthSareen <parth0612@live.com>
2019-01-08 22:12:31 -0500

    added RNN structure and contents

--- RealMachineLearning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/myCnn.py
+++ RealMachineLearning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/myCnn.py
@@ -66,7 +66,10 @@ test_set = test_datagen.flow_from_directory(
 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=8000,
-        epochs=25,
-        validation_data=test_set,
-        validation_steps=2000)
+        steps_per_epoch = (8000/32),
+        epochs = 25,
+        validation_data = test_set,
+        validation_steps = (2000/32),
+        use_multiprocessing = True)
+
+# Part 3 - Making new prediction
